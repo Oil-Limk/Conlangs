ee9fb1750f6e6e90a4c7d0cb8ef93b28b0b930f0
Oil Limk <camilo.urzua@ug.uchile.cl>
2024-06-25 16:05:42 -0400

    Fix[js]: Parcial solution implemented

--- src/templates/template.js
+++ src/templates/template.js
@@ -1,10 +1,5 @@
-const change_to_default = () => {
-    this.src = "https://oil-limk.github.io/Conlangs/src/img/flags.png";
-    this.onerror = "";
-}
-
 const imgs = document.getElementsByTagName("img");
 
 for(let i=0;i<imgs.length;i++){
-    imgs[i].onerror = change_to_default;
+    imgs[i].onerror = "this.src='https://oil-limk.github.io/Conlangs/src/img/flags.png';this.onerror='';"
 }
