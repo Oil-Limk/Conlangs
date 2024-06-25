31b0676d0a89f93a3f83ec7d5306b0827125623b
Oil Limk <camilo.urzua@ug.uchile.cl>
2024-06-25 16:14:29 -0400

    Fix: No default img implemented

--- src/templates/template.js
+++
@@ -1,5 +0,0 @@
-const imgs = document.getElementsByTagName("img");
-
-for(let i=0;i<imgs.length;i++){
-    imgs[i].onerror = "this.src='https://oil-limk.github.io/Conlangs/src/img/flags.png';this.onerror='';"
-}
